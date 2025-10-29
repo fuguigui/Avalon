da2aa5dd5e99c55a87310742e4a2f37cfc3fe716
fuguigui <fgr079@126.com>
2025-10-29 07:12:38 +0100

    Ignore npm build output.

--- functions/src/static/chunks/src_app_admin_create_page_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_b61142e2._.js",
-  "static/chunks/node_modules_788a1ee6._.js"
-],
-    source: "dynamic"
-});
